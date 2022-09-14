6118e7a25061d4e45eb572d5f0ef9d94afa29265
alberto8812 <cvelascosaavedra@gmail.com>
2022-09-14 16:12:24 -0500

    update data base product

--- api/service/allproductpets/getAllProductService.js
+++ api/service/allproductpets/getAllProductService.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
-
+const {db}=require('../../database/db')
+const {Product}=db.models
 
 const getAllProductService = async () => {
     const apiUrl =  (await axios.get(`https://pet-elegant.herokuapp.com/api/products`)).data
@@ -7,7 +8,7 @@ const getAllProductService = async () => {
         return{
           
             name: e.name,
-            description: e.description,
+            detail: e.description,
             price: e.price,
             stock: e.stock,
             sales: e.sales,
@@ -16,8 +17,19 @@ const getAllProductService = async () => {
            
         }
     })
+
+    for (const iterator of apiInfo) {
+
+        dbCreateProducts=await Product.findOrCreate({
+        where:{name: iterator.name}
+        })
+    }
+
 return apiInfo
 }
+
+
+
 
 module.exports = {
     getAllProductService

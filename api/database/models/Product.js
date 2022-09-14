58efd693eab917dd64422878d66ccc6d23257dfa
dianaibarra98 <dianaangelicaibarraparada@gmail.com>
2022-09-14 15:34:37 -0500

    arrelo en los DataTypes

--- api/database/models/Product.js
+++ api/database/models/Product.js
@@ -4,8 +4,9 @@ module.exports = (sequelize) => {
   sequelize.define('product', {
 
     id: {
-      type: DataTypes.STRING,
-      primaryKey: true,   //revisar
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,   
       allowNull: false
     }, 
 
@@ -34,6 +35,11 @@ module.exports = (sequelize) => {
     image: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+
+    rating:{
+      type: DataTypes.FLOAT,
+      allowNull: false, 
     }
 
   })

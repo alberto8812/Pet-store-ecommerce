f3b94f1e42e2949ba1d11712dea5c5eb56281cb4
Ramon181 <102682605+Ramon181@users.noreply.github.com>
2022-09-14 11:26:01 -0500

    client: adelanto de la navbar. RH

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/User/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
      <h1>PetLove</h1>
     </div>
   );

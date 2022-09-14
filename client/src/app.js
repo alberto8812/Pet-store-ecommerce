3668cc0792e39b05801febe040495afe92774c43
Dario Ponce <daroponce21@gmail.com>
2022-09-14 14:59:22 -0300

    Ahora si no habia guardado el App.Js del client, y borre un App.js que estaba fuera del proy q solo tenia un comentario

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,26 @@
+import { Profiler } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import NotFound from './components/User/404NotFound';
+import AboutUs from './components/User/AboutUs';
+import Detail from './components/User/Detail';
+import Home from './components/User/Home'
+import PaymentGateway from './components/User/PaymentGateway';
+import Registration from './components/User/Registration';
 
 function App() {
   return (
     <div className="App">
-     <h1>PetLove</h1>
+     <Routes>
+        <Route exact path={'/'} element={<Home/>}/>
+        <Route exact path={'/detail/:id'} element={<Detail/>}/>
+        <Route exact path={'/aboutus'} element={<AboutUs/>}/>
+        <Route exact path={'/registration'} element={<Registration/>}/>
+        <Route exact path={'/profile'} element={<Profiler/>}/>
+        <Route exact path={'/registration'} element={<Registration/>}/>
+        <Route exact path={'/paymentgateway'} element={<PaymentGateway/>}/>
+        <Route path={'*'} element={<NotFound/>}/>
+    </Routes>
     </div>
   );
 }

bbd5a3f37eb7d834f531fe81c88c72e46fdfe8dd
Dario Ponce <daroponce21@gmail.com>
2022-09-15 13:02:43 -0300

    se renderiza componente Card!!

--- client/src/components/User/Home/Home.jsx
+++ client/src/components/User/Home/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import LogIn from '../Login/LogIn';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { ComponentCard } from '../ComponentCard/ComponentCard';
 import Pagination from '../Pagination/Pagination';
 
 export default function Home() {
@@ -8,25 +9,32 @@ export default function Home() {
 SUPONGO QUE ESTA BIEN PERO HASTA QUE NO HAYA ALGO QUE MOSTRAR NO SABREMOS */
 
 
-//  const allProducts = useSelector(state => state.allProducts)
+  const allProducts = useSelector(state => state.allProducts)
 
     //PAGINADO
-//  const [currentPage, setCurrentPage] = useState(1);
-//  const [animalsPerPage, setAnimalsPerPage] = useState(5); // Hasta 5 cards por pag
-//  const indexLastAnimal = currentPage * animalsPerPage; 
-//  const indexFirstAnimal = indexLastAnimal - animalsPerPage; 
-//  const animalsInCurrentPage = allProducts.slice(indexFirstAnimal, indexLastAnimal);  //CHEQUEAR QUE STATE PUSIERON EN EL REDUCER !!!
+  const [currentPage, setCurrentPage] = useState(1);
+  const [animalsPerPage, setAnimalsPerPage] = useState(5); // Hasta 5 cards por pag
+  const indexLastAnimal = currentPage * animalsPerPage; 
+  const indexFirstAnimal = indexLastAnimal - animalsPerPage; 
+const animalsInCurrentPage = allProducts.slice(indexFirstAnimal, indexLastAnimal);  //CHEQUEAR QUE STATE PUSIERON EN EL REDUCER !!!
 
-//  const pagination = (pageNumber) => {
-//      setCurrentPage(pageNumber);
-//  }
+  const pagination = (pageNumber) => {
+      setCurrentPage(pageNumber);
+ }
 
   return (
       <div>
+<h1>ESTAMOS EN HOME</h1>
+<h1>PET LOVE STORE</h1>
+
 
         <div>
-           {/*<Pagination animalsPerPage={animalsPerPage} pagination={pagination} allProducts={allProducts.length} currentPage={currentPage} setCurrentPage={setCurrentPage}/>*/}
+           <Pagination animalsPerPage={animalsPerPage} pagination={pagination} allProducts={allProducts.length} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
         </div>
+<div>
+<ComponentCard animalsInCurrentPage={animalsInCurrentPage}/>
+  
+</div>
 
       </div>
   )
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,3 +1,24 @@
+import axios from "axios";
+
+export function getProducts(name) {
+    return async function (dispatch) {
+        try {
+            if (name) {
+                return axios.get('http://localhost:3001/products?name=' + name)
+                    .then(res => dispatch({ type: 'GET_ALL_PRODUCTS', payload: res.data }))
+                    .catch(err => dispatch({ type: 'GET_ALL_PRODUCTS', payload: err.data }))
+            }
+            let json = await axios.get('http://localhost:3001/products', {})
+            return dispatch({
+                type: 'GET_ALL_PRODUCTS',
+                payload: json.data
+            })
+        } catch (err) {
+            console.log(err)
+            }
+        }
+
+    }
 
 
 export function postProduct(payload) {

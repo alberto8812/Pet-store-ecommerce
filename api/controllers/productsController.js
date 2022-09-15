b4d0663b307e11e9844508d38f41c29b30399f6e
alberto8812 <cvelascosaavedra@gmail.com>
2022-09-15 14:51:10 -0500

    f(rute detail made)

--- api/controllers/productsController.js
+++ api/controllers/productsController.js
@@ -1,5 +1,10 @@
 const {getAllProductApiService,getAllProudctsservice,getBasicProducts}=require('../service/allproductpets/getAllProductService')
 
+const {getDbProductDetail}=require('../service/allproductpets/getProductDetailService')
+
+
+
+
 const  getAllProductsData=async(req,res)=>{
      const createBasicProducts=await getBasicProducts()//crea una base de las categoria,generos y productos en el db
      const allcreateproducts=await getAllProductApiService()//llama a la api para traer los accesorios de perros
@@ -7,6 +12,18 @@ const  getAllProductsData=async(req,res)=>{
      res.status(200).json(getallproducts)
 }
 
+
+
+const getProductDetail=async(req,res)=>{
+    try {
+        const getproductDetail=await getDbProductDetail(req)
+        res.status(200).json( getproductDetail) 
+    } catch (error) {
+        res.status(500).json({msg:error.message})
+    }
+}
+
 module.exports={
-    getAllProductsData
+    getAllProductsData,
+    getProductDetail
 }

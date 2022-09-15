8188e553a1c510d1ac7c6aba2509548f32624ca9
Ramon181 <102682605+Ramon181@users.noreply.github.com>
2022-09-15 08:06:04 -0500

    actulice

--- api/V1/routes/usersRouter.js
+++ api/V1/routes/usersRouter.js
@@ -3,7 +3,6 @@ const { getUserLogin } = require("../../controllers/usersController");
 const express=require("express"),
       router=express.Router();
 
-router
-      .get('/', getUserLogin) 
+routers.get('/', getUserLogin) 
 
 module.exports=router;

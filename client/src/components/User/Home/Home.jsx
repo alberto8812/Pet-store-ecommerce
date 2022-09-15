0c90e16d6ed9a5dd32af964257180740526d0c37
Ailen De Santis <ailenm.de.santis@gmail.com>
2022-09-15 17:09:03 -0300

    tratando de traerme cambios

--- client/src/components/User/Home/Home.jsx
+++ client/src/components/User/Home/Home.jsx
@@ -15,15 +15,15 @@ SUPONGO QUE ESTA BIEN PERO HASTA QUE NO HAYA ALGO QUE MOSTRAR NO SABREMOS */
   const allProducts = useSelector(state => state.allProducts)
 
     //PAGINADO
-  const [currentPage, setCurrentPage] = useState(1);
-  const [animalsPerPage, setAnimalsPerPage] = useState(5); // Hasta 5 cards por pag
-  const indexLastAnimal = currentPage * animalsPerPage; 
-  const indexFirstAnimal = indexLastAnimal - animalsPerPage; 
-const animalsInCurrentPage = allProducts.slice(indexFirstAnimal, indexLastAnimal);  //CHEQUEAR QUE STATE PUSIERON EN EL REDUCER !!!
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [animalsPerPage, setAnimalsPerPage] = useState(5); // Hasta 5 cards por pag
+//   const indexLastAnimal = currentPage * animalsPerPage; 
+//   const indexFirstAnimal = indexLastAnimal - animalsPerPage; 
+// const animalsInCurrentPage = allProducts.slice(indexFirstAnimal, indexLastAnimal);  //CHEQUEAR QUE STATE PUSIERON EN EL REDUCER !!!
 
-  const pagination = (pageNumber) => {
-      setCurrentPage(pageNumber);
- }
+//   const pagination = (pageNumber) => {
+//       setCurrentPage(pageNumber);
+//  }
 
   return (
 
@@ -31,16 +31,18 @@ const animalsInCurrentPage = allProducts.slice(indexFirstAnimal, indexLastAnimal
         <div className='container-wrap'>
 
        
-<h1>ESTAMOS EN EL HOME</h1>
+  <h1>ESTAMOS EN EL HOME</h1>
 
 
         <div>
-           <Pagination animalsPerPage={animalsPerPage} pagination={pagination} allProducts={allProducts.length} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+           {/* <Pagination animalsPerPage={animalsPerPage} pagination={pagination} allProducts={allProducts.length} currentPage={currentPage} setCurrentPage={setCurrentPage}/> */}
         </div>
 
-        </div>
+      <div>
       <Footer/>
-  )
+      </div>
 
+      </div>
+      </div>
+  )
 }
-//agrego

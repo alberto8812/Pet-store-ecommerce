577e95d2c35336acd8cc1a40f567051ca144bd40
dianaibarra98 <dianaangelicaibarraparada@gmail.com>
2022-09-22 17:07:31 -0500

    arreglo enum

--- api/database/models/Review.js
+++ api/database/models/Review.js
@@ -13,7 +13,7 @@ module.exports = (sequelize) =>{
       allowNull: false
     }, 
     punctuation:{
-      type: DataTypes.ENUM(1, 2, 3, 4, 5)
+      type: DataTypes.ENUM('1', '2', '3', '4', '5')
     }
   })
 }

958139156f6bf01f1069e3ffd064f70b17aee91a
Dario Ponce <daroponce21@gmail.com>
2022-09-28 14:11:22 -0300

    al fin anda y manda al back, me falta mejorarlo para que mande mas cosas pero este anda

--- api/service/users/postUserPaymentService.js
+++ api/service/users/postUserPaymentService.js
@@ -3,11 +3,11 @@ const Stripe = require("stripe");
 const stripe = new Stripe("sk_test_51LkfWEIzGpa9z0EFyh2s6ZllQ7WWYamRZ73C8E8BvU8reKDZs1PtQuXs0BcBdjvWTP5mcIysLhleVAeajwYByS6Q00vuHh06XT");
 
 const postUserPaymentService =async(req)=>{
-    const { id, amount } = req.body;
+    const { id, amount, description } = req.body;
     const payment = await stripe.paymentIntents.create({
         amount,
         currency: "USD",
-        description: "Gaming Keyboard",
+        description,
         payment_method: id,
         confirm: true, //confirm the payment at the same time
   
--- client/src/components/User/PaymentGateway/PaymentGateway.jsx
+++ client/src/components/User/PaymentGateway/PaymentGateway.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import {withAuthenticationRequired} from '@auth0/auth0-react'
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import "bootswatch/dist/pulse/bootstrap.min.css";
 import './PaymentGateway.css'
 //import { loadStripe } from "@stripe/stripe-js";
+import StripeCheckout from 'react-stripe-checkout'
 
 import {
     //Elements,
@@ -17,7 +18,8 @@ import Loading from "../Loading/Loading";
 
 //const stripePromise = loadStripe("pk_test_51LkfWEIzGpa9z0EFC6OqfUFPRBmrUIS1nZVezBHgqSh6GBtJ3x5whj06EuCkgwBhls2xwc3M8UI9JKxid7o7Zzni00BiLqFS7P");
 
-export default withAuthenticationRequired (function PaymentGateway({ image, name, stock, price, id, category, genre, age }) {
+export default withAuthenticationRequired(function PaymentGateway({ image, name, stock, price, id, category, genre, age }) {
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
     const stripe = useStripe();
     const elements = useElements();
 
@@ -26,16 +28,19 @@ export default withAuthenticationRequired (function PaymentGateway({ image, name
 
     let listCart = [];
     let totalCart = 0;
-  
+
     Object.keys(productsInTheCart).forEach(product => {
-      totalCart += productsInTheCart[product].quantity * productsInTheCart[product].price;
-      listCart.push(productsInTheCart[product]);
-  });
-  
-    function totalPrice(price, item){
+        totalCart += productsInTheCart[product].quantity * productsInTheCart[product].price;
+        listCart.push(productsInTheCart[product]);
+    });
+
+    const idList = listCart.map(e => e.id).toString()
+
+    function totalPrice(price, item) {
         return Number(price * item).toLocaleString('en-US');
     };
-  
+
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -48,14 +53,21 @@ export default withAuthenticationRequired (function PaymentGateway({ image, name
         setLoading(true);
 
         if (!error) {
-            // console.log(paymentMethod)
+            console.log(paymentMethod)
             const { id } = paymentMethod;
             try {
+                const token = await getAccessTokenSilently()
                 const { data } = await axios.post(
                     "http://localhost:3001/loginUsers/checkoutpayment",
                     {
                         id,
-                        amount: 10000, //cents
+                        amount: totalCart * 100,
+                        description: idList
+                    },
+                    {
+                        headers: {
+                            authorization: `Bearer ${token}`
+                        },
                     }
                 );
                 console.log(data);
@@ -63,12 +75,11 @@ export default withAuthenticationRequired (function PaymentGateway({ image, name
                 elements.getElement(CardElement).clear();
             } catch (error) {
                 console.log(error);
-            } 
+            }
             setLoading(false);
         }
     };
 
-    console.log(!stripe || loading);
 
     return (
         <div className="m7">
@@ -85,44 +96,44 @@ export default withAuthenticationRequired (function PaymentGateway({ image, name
                     <div className="product-head desk">
                         <div className="clear"></div>
                     </div>
-                    
+
                     <div className="table">
-                    {
-                  listCart.map((item) => {
-                      return(
-                          <div>
-                              <h5 className="name">{item.name}</h5>
-                              <img className="image" src={item.image} alt={item.name} height='50px' width='50px'/>
-                              <div className="product-info">
-                              <h5 className="name">
-                                <a href={`/products/detail/${id}`} target='_blank'>Go to product details</a>
-                              </h5>
-                              <div className="description">
-                                <ul>
-                                    <li>
-                                        <strong>Age:</strong>
-                                        {item.age}
-                                    </li>
-                                    <li>
-                                        <strong>Id Product:</strong>
-                                        {item.id}
-                                    </li>
-                                </ul>
-                            </div>
-                            </div>
-                            <div className="quantity" data-id="0">
-                            <div className="qty">
-                                <label type="number" id="cantidad_13854" pattern="[0-9]+" className="count" /> 
-                                {item.quantity} 
-                            </div>
-                            </div>
-                            <span>Total {totalPrice(item.price, item.quantity)} $</span>
-                          </div>
-                      )
-                  })
-              }
-                </div>
-                    
+                        {
+                            listCart.map((item) => {
+                                return (
+                                    <div>
+                                        <h5 className="name">{item.name}</h5>
+                                        <img className="image" src={item.image} alt={item.name} height='50px' width='50px' />
+                                        <div className="product-info">
+                                            <h5 className="name">
+                                                <a href={`/products/detail/${id}`} target='_blank'>Go to product details</a>
+                                            </h5>
+                                            <div className="description">
+                                                <ul>
+                                                    <li>
+                                                        <strong>Age:</strong>
+                                                        {item.age}
+                                                    </li>
+                                                    <li>
+                                                        <strong>Id Product:</strong>
+                                                        {item.id}
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div className="quantity" data-id="0">
+                                            <div className="qty">
+                                                <label type="number" id="cantidad_13854" pattern="[0-9]+" className="count" />
+                                                {item.quantity}
+                                            </div>
+                                        </div>
+                                        <span>Total {totalPrice(item.price, item.quantity)} $</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
                 </div>
             </section>
             {<section className="cart-totals-container">
@@ -133,9 +144,10 @@ export default withAuthenticationRequired (function PaymentGateway({ image, name
                             {/* <h3 className="cart-totals">Total: 100$</h3> */}
                             <h3 className="cart-totals">Total: {Number(totalCart).toLocaleString('en-US')}$</h3>
                             <div className="form-group">
-                                <CardElement className="pagos"/>
+                                <CardElement
+                                />
                             </div>
-                          <button disabled={!stripe} className="btn btn-success">
+                            <button disabled={!stripe} className="btn btn-success">
                                 {loading ? (
                                     <div className="spinner-border text-light" role="status">
                                         <span className="sr-only"></span>
@@ -157,5 +169,5 @@ export default withAuthenticationRequired (function PaymentGateway({ image, name
             </div>
         </div >
     )
-},{onRedirecting:()=><Loading/>});
+}, { onRedirecting: () => <Loading /> });
 

f5e8d4a25e231b076998a7eef3fab2fa849e4d2d
alberto88 <cvelascosaavedra@gmail.com>
2022-09-28 12:14:15 -0500

    chpping admin user update with backend date

--- client/src/components/Admin/Users/Users.jsx
+++ client/src/components/Admin/Users/Users.jsx
@@ -3,13 +3,16 @@ import { useDispatch } from "react-redux";
 import { useAuth0 } from '@auth0/auth0-react'//libreia Auth0
 import { getCustomerShopping } from '../../../redux/actions';
 import { useSelector } from "react-redux";
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 
 const Users = () => {
   const dispatch = useDispatch();
   const {isAuthenticated,getAccessTokenSilently}=useAuth0()//componete de hook auth0
-  const custumerData = useSelector(state => state.customerShopping);
-
-  console.log(custumerData)
+  const custumerDataDb = useSelector(state => state.customerShopping);
+  const custumerData=custumerDataDb!==undefined?custumerDataDb:[];
+  let rows=[]
+console.log(custumerData)
   
   useEffect(() => {
     const getToken=async()=>{
@@ -28,13 +31,85 @@ const Users = () => {
 
   }, [])
 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    {
+      field: 'email',
+      headerName: 'EMAIL',
+      width: 250,
+      editable: true,
+    },
+    {
+      field: 'firstName',
+      headerName: 'FirstName',
+      width: 250,
+      editable: true,
+    },
+    {
+      field: 'invoice',
+      headerName: 'Invoice',
+      //type: 'number',
+      width: 110,
+      editable: true,
+    },
+    ,
+    {
+      field: 'total',
+      headerName: 'Total',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      //type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'quantity',
+      headerName: 'Quantity',
+       type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'product',
+      headerName: 'Product',
+      // type: 'number',
+      width: 450,
+      editable: true,
+    },
+    
+  ];
+  
+  if(custumerData.length){
+ for (const users of custumerData) {
+  for (const sales of users.sales) {
+    for (const saleDetails of sales.saleDetails) {
+      for (const products of  saleDetails.products) {
+        let dataRow={id:sales.invoice, email:users.email, firstName:users.name,invoice:sales.invoice,status:sales.status,total:sales.total,quantity:saleDetails.quantity,product:products.name }
+        rows.push(dataRow)       
+      }     
+    }      
+  } 
+ }
 
-
+  }
 
   return (
-    <div>
-      <h3>users</h3>
-    </div>
+    <Box sx={{ height: 500, width: '100%'}}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        //checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box>
   )
 }
 

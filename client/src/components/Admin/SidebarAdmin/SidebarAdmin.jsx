b4d29985dfa1a401ef1d782240f75c615d6f0e01
dianaibarra98 <dianaangelicaibarraparada@gmail.com>
2022-09-27 21:18:14 -0500

    dashboard

--- client/src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
+++ client/src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
@@ -6,20 +6,22 @@ import * as FaIcons from 'react-icons/fa'
 const SidebarAdmin = () => {
   return (
     <div className='sidebar-admin'>
-      <ul>
-        <li>
-          <Link to="/logs/" ><FaIcons.FaHome/> Home de Admin</Link>          
-        </li>
-        <li>
-          <Link to="/logs/statistics"><FaIcons.FaChartBar/> Statistics </Link>          
-        </li>
-        <li>
-          <Link to="/logs/users"><FaIcons.FaUsers/> Users</Link>          
-        </li>
-        <li>
-          <Link to="/logs/productsAdm"> <FaIcons.FaBone/> Products</Link>          
-        </li>
-      </ul>
+        <div>
+          <ul>
+            <li>
+              <Link to="/logs/" ><FaIcons.FaHome/> Home de Admin</Link>          
+            </li>
+            <li>
+              <Link to="/logs/statistics"><FaIcons.FaChartBar/> Statistics </Link>          
+            </li>
+            <li>
+              <Link to="/logs/users"><FaIcons.FaUsers/> Users</Link>          
+            </li>
+            <li>
+              <Link to="/logs/productsAdm"> <FaIcons.FaBone/> Products</Link>          
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }

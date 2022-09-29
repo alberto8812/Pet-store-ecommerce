96dbc9aae676b6d46922119be18bcc3841e08508
dianaibarra98 <dianaangelicaibarraparada@gmail.com>
2022-09-28 19:59:10 -0500

    tablas users

--- client/src/components/Admin/ProductsAdm/ProductsAdm.jsx
+++ client/src/components/Admin/ProductsAdm/ProductsAdm.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const ProductsAdm = () => {
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+function createData(name, id, price, stock, category) {
+  return { name, id, price, stock, category };
+}
+
+
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  
+
+];
+
+const ProductsAdm= () => {
+
   return (
-    <div>
-      <h3>Products</h3>
-    </div>
-  )
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Product Name</StyledTableCell>
+            <StyledTableCell align="right">ID</StyledTableCell>
+            <StyledTableCell align="right">Price&nbsp;</StyledTableCell>
+            <StyledTableCell align="right">Stock&nbsp;</StyledTableCell>
+            <StyledTableCell align="right">Category&nbsp;</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.id}</StyledTableCell>
+              <StyledTableCell align="right">{row.price}</StyledTableCell>
+              <StyledTableCell align="right">{row.stock}</StyledTableCell>
+              <StyledTableCell align="right">{row.category}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default ProductsAdm

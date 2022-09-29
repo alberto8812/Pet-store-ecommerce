3f28df1eb8b5bc256e656f32a802f9e04973961b
alberto88 <cvelascosaavedra@gmail.com>
2022-09-29 12:55:39 -0500

    cambio en userRouter

--- api/V1/routes/usersRouter.js
+++ api/V1/routes/usersRouter.js
@@ -1,6 +1,5 @@
 const express=require("express"),
-      router=express.Router(),
-      {jwtCheck,checkpermissions}=require('../../middleware/jwtLoginUser');
+      router=express.Router();
 const { getUserLogin, postUserData, postComment,postUserCart,postUserPayment } = require("../../controllers/usersController");
 
 

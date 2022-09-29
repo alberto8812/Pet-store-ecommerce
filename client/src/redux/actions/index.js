64a9922f233accbacfbf0a30971709b5041d6214
fgastiarena <mf.gastiarena@gmail.com>
2022-09-29 12:10:21 -0300

    conflictos arreglados

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 import {
-    GET_ALL_PRODUCTS,GET_DETAILS,SORT_BY_PRICE,
-    SEARCH_BY_NAME,ADD_FAVORITE,REMOVE_FAVORITE,
-    ADD_TO_CART,GET_NUMBER_CART,INCREASE_QUANTITY,
-    DECREASE_QUANTITY,UPDATE_CART,DELETE_CART,
-    ADD_COMMENT,REFRESH_CART,GET_ADMINROLL,
-    GET_GRAPHICS_DATA, DELETE_PRODUCT,
+    GET_ALL_PRODUCTS,
+    GET_DETAILS,
+    SORT_BY_PRICE,
+    SEARCH_BY_NAME,
+    ADD_FAVORITE,
+    REMOVE_FAVORITE,
+    ADD_TO_CART,
+    GET_NUMBER_CART,
+    INCREASE_QUANTITY,
+    DECREASE_QUANTITY,
+    UPDATE_CART,
+    DELETE_CART,
+    ADD_COMMENT,
+    REFRESH_CART,
+    GET_ADMINROLL,
+    GET_GRAPHICS_DATA,
+    DELETE_PRODUCT,
     GET_CUSTOMER_SHOPPING
 } from "./constants";
 
@@ -73,7 +84,6 @@ export function sortByPrice(payload) {
 };
 
 export function addToCart(payload) {
-    console.log('SOY EL ADDTOCART PAYLOAD', payload);
     return {
         type: ADD_TO_CART,
         payload
@@ -134,16 +144,16 @@ export function refreshCart(payload) {
 };
 
 ////////////////////////************Admind*******************/////////////////////////////////// /
- 
+
 
 
 
 //verifica que tipo de roll de usuario
 
-export const getRollAdmin=(token)=>{
+export const getRollAdmin = (token) => {
     return async(dispatch) => {
         try {
-            const { data } = await axios.get(`http://localhost:3001/loginAdmin`,token);
+            const { data } = await axios.get(`http://localhost:3001/loginAdmin`, token);
             return dispatch({
                 type: GET_ADMINROLL,
                 payload: data,
@@ -152,13 +162,13 @@ export const getRollAdmin=(token)=>{
             console.error(err);
         }
     };
- }
+}
 
- //consigue todos los datos de las graficas 
- export const getgraphicsData=(token)=>{
+//consigue todos los datos de las graficas 
+export const getgraphicsData = (token) => {
     return async(dispatch) => {
         try {
-            const { data } = await axios.get(`http://localhost:3001/loginAdmin/graphics`,token);
+            const { data } = await axios.get(`http://localhost:3001/loginAdmin/graphics`, token);
             return dispatch({
                 type: GET_GRAPHICS_DATA,
                 payload: data,
@@ -167,13 +177,13 @@ export const getRollAdmin=(token)=>{
             console.error(err);
         }
     };
- }
+}
 
 //consigue todos los datos de los usuarios que realizaron las compras
-export const getCustomerShopping=(token)=>{
+export const getCustomerShopping = (token) => {
     return async(dispatch) => {
         try {
-            const { data } = await axios.get(`http://localhost:3001/loginAdmin/customerShopping`,token);
+            const { data } = await axios.get(`http://localhost:3001/loginAdmin/customerShopping`, token);
             return dispatch({
                 type: GET_CUSTOMER_SHOPPING,
                 payload: data,
@@ -184,13 +194,14 @@ export const getCustomerShopping=(token)=>{
     };
 }
 
- export function deleteProducts(id, setFlag) {
+export function deleteProducts(id, setFlag) {
     return async(dispatch) => {
         try {
             return axios.delete(`http://localhost:3001/loginAdmin/delete/${id}`)
                 .then(res => {
                     setFlag((flag) => !flag)
-                    return dispatch({ type: DELETE_PRODUCT, payload: res.data })})
+                    return dispatch({ type: DELETE_PRODUCT, payload: res.data })
+                })
                 .catch(err => dispatch({ type: DELETE_PRODUCT, payload: err.data }))
         } catch (error) {
             console.log(error)

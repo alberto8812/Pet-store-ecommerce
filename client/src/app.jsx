5b7d0c5503bce7b8d01d60586b61a1fcc9966a22
alberto88 <cvelascosaavedra@gmail.com>
2022-09-29 19:43:19 -0500

    update client folder

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -50,7 +50,7 @@ function App() {
         <Route path={"/carrito"} element={<Carrito />} />
       </Routes> 
       {/* dashboard de admin */}
-     {/* ////////< NavbarAdmin /> */ }
+     {/* < NavbarAdmin /> */ }
     
       <Routes>
       <Route  path={"/admin/"} element={< SidebarAdmin />}>
